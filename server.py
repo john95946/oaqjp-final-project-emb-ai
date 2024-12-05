1c04f33f52d10e7c97c6cf23e0bb674abad4b71c
John <fischerj1517@gmail.com>
2024-12-04 19:18:44 -0500

    Update adds the server

---
+++ server.py
@@ -0,0 +1,32 @@
+'''
+emotion detector web application
+'''
+from flask import Flask, render_template, request
+from EmotionDetection.emotion_detection import emotion_detector
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    '''display the index template'''
+    return render_template('index.html')
+
+@app.route('/emotionDetector')
+def get_the_emotion():
+    '''detect the emotions'''
+    text_to_analyze = request.args.get("textToAnalyze")
+    response = emotion_detector(text_to_analyze)
+
+    anger = response['anger']
+    disgust = response['disgust']
+    fear = response['fear']
+    joy = response['joy']
+    sadness = response['sadness']
+    dominant = response['dominant_emotion']
+    return f"For the given statement, the system response is 'anger': {anger}, " + \
+        f"'disgust': {disgust}, 'fear': {fear}, 'joy': {joy}, 'sadness': {sadness}.  " + \
+        f"The dominant emotion is <b>{dominant}</b>."
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
